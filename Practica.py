1e3609f88ada9a895f2aed05a80c5d481585ff9d
AlexisHuaman <78916588+AlexisHuaman@users.noreply.github.com>
2024-04-16 20:14:42 -0500

    Update Practica.py

--- Practica.py
+++ Practica.py
@@ -1,2 +1,20 @@
 # Practica-Calificada-01
-hola
+contador = 0
+continuar = ""
+while continuar == "SI":
+    #Ingresar empleados
+    print("Creando un nuevo usuario:")
+    empleado_Id = input("Id: ")
+    empleado_Nombre = input("Nombre completo: ")
+    empleado_Dni = input("Dni: ")
+    empleado_Telefono = input("Telefono: ")
+    empleado_Tiempo = input("Tiempo trabajando (meses): ")
+    empleado_Cargo = input("Cargo: ")
+    
+    print("Quiere ingresar un nuevo empleado al registro (Si/No)")
+    auxiliar = input()
+    auxiliar.upper()
+    print("continuar")
+    continuar == auxiliar
+
+print("Acabo el")
